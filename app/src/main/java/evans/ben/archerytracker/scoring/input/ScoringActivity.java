97a6f3dd67f95865f6b97a4131edba88e7146921
Ben Evans <ben.j.evans19@gmail.com>
2020-09-21 15:20:29 +0100

    Scoring keyboard added and ET/RT calculations automated, minor cosmetic changes to fragment_home.xml.

--- app/src/main/java/evans/ben/archerytracker/scoring/input/ScoringActivity.java
+++ app/src/main/java/evans/ben/archerytracker/scoring/input/ScoringActivity.java
@@ -1,19 +1,30 @@
 package evans.ben.archerytracker.scoring.input;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 import evans.ben.archerytracker.R;
 
 public class ScoringActivity extends AppCompatActivity {
     private int selectedArrow;
+    private int arrowsEnd;
+    private int rows;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +37,14 @@ public class ScoringActivity extends AppCompatActivity {
         Distance distance = intent.getParcelableExtra("distance");
         assert distance != null;
         distanceTextView.setText(distance.getDistance());
+        arrowsEnd = distance.getArrowsEnd();
 
         int arrowsEnd = distance.getArrowsEnd();
-        int rows = Integer.parseInt(distance.getArrowsAtDistance()) / arrowsEnd + 1;
+        rows = Integer.parseInt(distance.getArrowsAtDistance()) / arrowsEnd + 1;
+
         // Making the table and setting up on click listeners for arrow value textViews
         init(arrowsEnd, rows);
+
         // Setting up so that on opening the first empty arrow textView is selected
         int check = firstSelectedArrow(arrowsEnd, rows);
         // In case all arrow textViews are filled
@@ -38,8 +52,22 @@ public class ScoringActivity extends AppCompatActivity {
             selectedArrow = 10;
         }
 
+        initKeyboard(distance.getScoringStyle());
+
+        FloatingActionButton backspaceButton = findViewById(R.id.scoring_del_button);
+        backspaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView current = findViewById(selectedArrow);
+                current.setText("");
+                current.setBackgroundResource(R.drawable.cell_shape_selected);
+                updateTotals(selectedArrow / 10);
+            }
+        });
+
     }
 
+    // This methods creates the table for the arrow values to go in
     private void init(int arrowsEnd, int rows) {
         TableLayout scoringTable = findViewById(R.id.scoring_table);
         // Used so that table stretches to fit screen
@@ -47,8 +75,6 @@ public class ScoringActivity extends AppCompatActivity {
 
         // So that text size can be altered for all textViews easily
         int textSize = 20;
-        // So that padding can be altered for all textViews easily
-        int padding = 5;
 
         for (int i  = 0; i < rows; i++) {
             TableRow row = new TableRow(this);
@@ -73,7 +99,6 @@ public class ScoringActivity extends AppCompatActivity {
                 arrowsHeader.setLayoutParams(headerLp);
                 arrowsHeader.setTextSize(textSize);
                 arrowsHeader.setTypeface(Typeface.DEFAULT_BOLD);
-                arrowsHeader.setPadding(padding, padding, padding, padding);
                 arrowsHeader.setBackgroundResource(R.drawable.cell_shape_thick_bottom);
                 arrowsHeader.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
                 arrowsHeader.setText(R.string.arrows);
@@ -84,7 +109,6 @@ public class ScoringActivity extends AppCompatActivity {
                     final TextView arrow = new TextView(this);
                     arrow.setLayoutParams(lp);
                     arrow.setTextSize(textSize);
-                    arrow.setPadding(padding, padding, padding, padding );
                     // Setting borders for cells
                     arrow.setBackgroundResource(R.drawable.cell_shape);
 
@@ -114,7 +138,6 @@ public class ScoringActivity extends AppCompatActivity {
             TextView ET = new TextView(this);
             ET.setLayoutParams(totalsLp);
             ET.setTextSize(textSize);
-            ET.setPadding(padding, padding, padding, padding);
             ET.setBackgroundResource(R.drawable.cell_shape_thick_sides);
             ET.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
             ET.setTypeface(Typeface.DEFAULT_BOLD);
@@ -123,7 +146,6 @@ public class ScoringActivity extends AppCompatActivity {
             TextView RT = new TextView(this);
             RT.setLayoutParams(totalsLp);
             RT.setTextSize(textSize);
-            RT.setPadding(padding, padding, padding, padding);
             RT.setBackgroundResource(R.drawable.cell_shape);
             RT.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
             RT.setTypeface(Typeface.DEFAULT_BOLD);
@@ -148,6 +170,7 @@ public class ScoringActivity extends AppCompatActivity {
         }
     }
 
+    // This method is used to select the first empty arrow textView
     private int firstSelectedArrow(int arrowsEnd, int ends) {
         for (int i = 1; i < ends; i++) {
             for (int j = 0; j < arrowsEnd; j++) {
@@ -161,5 +184,252 @@ public class ScoringActivity extends AppCompatActivity {
             }
         }
         return 1;
+    }
+
+    // This method makes the keyboard depending on the scoring type
+    private void initKeyboard(int scoringType) {
+        TableLayout keyboard = findViewById(R.id.scoring_keyboard_table);
+        keyboard.setStretchAllColumns(true);
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        lp.weight = 1;
+        lp.setMargins(10,10,10,10);
+
+        if (scoringType == 0) {
+            TableRow tableRow1 = new TableRow(this);
+            tableRow1.setLayoutParams(lp);
+            // Adding all buttons
+            TextView button = makeButton("X", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("10", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("9", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("8", Color.RED, lp);
+            tableRow1.addView(button);
+            button = makeButton("7", Color.RED, lp);
+            tableRow1.addView(button);
+            button = makeButton("6", Color.BLUE, lp);
+            tableRow1.addView(button);
+            keyboard.addView(tableRow1);
+
+            // New row
+            TableRow tableRow2 = new TableRow(this);
+            tableRow2.setLayoutParams(lp);
+            button = makeButton("5", Color.BLUE, lp);
+            tableRow2.addView(button);
+            button = makeButton("4", Color.BLACK, lp);
+            tableRow2.addView(button);
+            button = makeButton("3", Color.BLACK, lp);
+            tableRow2.addView(button);
+            button = makeButton("2", Color.WHITE, lp);
+            tableRow2.addView(button);
+            button = makeButton("1", Color.WHITE, lp);
+            tableRow2.addView(button);
+            button = makeButton("M", Color.GREEN, lp);
+            tableRow2.addView(button);
+            keyboard.addView(tableRow2);
+        }
+        else if (scoringType == 1) {
+            TableRow tableRow1 = new TableRow(this);
+            tableRow1.setLayoutParams(lp);
+            // Adding all buttons
+            TextView button = makeButton("9", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("7", Color.RED, lp);
+            tableRow1.addView(button);
+            button = makeButton("5", Color.BLUE, lp);
+            tableRow1.addView(button);
+            button = makeButton("3", Color.BLACK, lp);
+            tableRow1.addView(button);
+            button = makeButton("1", Color.WHITE, lp);
+            tableRow1.addView(button);
+            button = makeButton("M", Color.GREEN, lp);
+            tableRow1.addView(button);
+            keyboard.addView(tableRow1);
+        }
+        else if (scoringType == 2) {
+            TableRow tableRow1 = new TableRow(this);
+            tableRow1.setLayoutParams(lp);
+            // Adding all buttons
+            TextView button = makeButton("10", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("9", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("8", Color.RED, lp);
+            tableRow1.addView(button);
+            button = makeButton("7", Color.RED, lp);
+            tableRow1.addView(button);
+            button = makeButton("6", Color.BLUE, lp);
+            tableRow1.addView(button);
+            keyboard.addView(tableRow1);
+
+            // New row
+            TableRow tableRow2 = new TableRow(this);
+            tableRow2.setLayoutParams(lp);
+            button = makeButton("5", Color.BLUE, lp);
+            tableRow2.addView(button);
+            button = makeButton("4", Color.BLACK, lp);
+            tableRow2.addView(button);
+            button = makeButton("3", Color.BLACK, lp);
+            tableRow2.addView(button);
+            button = makeButton("2", Color.WHITE, lp);
+            tableRow2.addView(button);
+            button = makeButton("1", Color.WHITE, lp);
+            tableRow2.addView(button);
+            button = makeButton("M", Color.GREEN, lp);
+            tableRow2.addView(button);
+            keyboard.addView(tableRow2);
+        }
+        else if (scoringType == 3) {
+            TableRow tableRow1 = new TableRow(this);
+            tableRow1.setLayoutParams(lp);
+            // Adding all buttons
+            TextView button = makeButton("10", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("9", Color.YELLOW, lp);
+            tableRow1.addView(button);
+            button = makeButton("8", Color.RED, lp);
+            tableRow1.addView(button);
+            button = makeButton("7", Color.RED, lp);
+            tableRow1.addView(button);
+            button = makeButton("6", Color.BLUE, lp);
+            tableRow1.addView(button);
+            button = makeButton("M", Color.GREEN, lp);
+            tableRow1.addView(button);
+            keyboard.addView(tableRow1);
+        }
+        else {
+            TableRow tableRow1 = new TableRow(this);
+            tableRow1.setLayoutParams(lp);
+            // Adding all buttons
+            TextView button = makeButton("5", Color.WHITE, lp);
+            tableRow1.addView(button);
+            button = makeButton("4", Color.BLACK, lp);
+            tableRow1.addView(button);
+            button = makeButton("3", Color.BLACK, lp);
+            tableRow1.addView(button);
+            button = makeButton("2", Color.BLACK, lp);
+            tableRow1.addView(button);
+            button = makeButton("1", Color.BLACK, lp);
+            tableRow1.addView(button);
+            button = makeButton("M", Color.GREEN, lp);
+            tableRow1.addView(button);
+            keyboard.addView(tableRow1);
+        }
+    }
+
+    /* This method makes a keyboard button, taking the value, colour of the button on input and
+       layout parameter on input */
+    private TextView makeButton(final String value, final int colour, TableRow.LayoutParams layoutParams) {
+        // Button appearance
+        Button button = new Button(this);
+        button.setLayoutParams(layoutParams);
+        button.setTextSize(20);
+        button.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        button.setText(value);
+        button.setBackgroundColor(colour);
+        if (colour == Color.BLACK || colour == Color.BLUE) {
+            button.setTextColor(Color.WHITE);
+        }
+
+        // On click listener for scoring
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView currentArrow = findViewById(selectedArrow);
+                currentArrow.setText(value);
+                //currentArrow.setBackgroundColor(colour);
+
+                // Moving which arrow is selected
+                currentArrow.setBackgroundResource(R.drawable.cell_shape);
+
+                // Calculating where we are on the table
+                int col = selectedArrow % 10;
+                int row = selectedArrow / 10;
+
+                if (col == arrowsEnd - 1 && row == rows - 1) {
+                    selectedArrow = 10;
+                }
+                else if (col == arrowsEnd - 1) {
+                    // For concatenating next selectedArrow value
+                    String newSelectedArrow = String.format("%s0", row + 1);
+                    selectedArrow = Integer.parseInt(newSelectedArrow);
+                    currentArrow = findViewById(selectedArrow);
+                    currentArrow.setBackgroundResource(R.drawable.cell_shape_selected);
+                }
+                else {
+                    selectedArrow += 1;
+                    currentArrow = findViewById(selectedArrow);
+                    currentArrow.setBackgroundResource(R.drawable.cell_shape_selected);
+                }
+
+                // Updating ET and RT
+                updateTotals(row);
+            }
+        });
+        return button;
+    }
+
+    // Updating ET and RT values
+    private void updateTotals(int row) {
+        for (int i = row; i < rows; i++) {
+            String ETID = String.format("20%s", i);
+            String RTID = String.format("30%s", i);
+            TextView currentET = findViewById(Integer.parseInt(ETID));
+            TextView currentRT = findViewById(Integer.parseInt(RTID));
+            // For the case where someone enters a miss
+            boolean miss = false;
+            // Calculating ET for this row
+            int rowET = 0;
+            for (int j = 0; j < arrowsEnd; j++) {
+                // Getting current arrow
+                String arrowID = String.format("%s%s", i, j);
+                TextView arrow = findViewById(Integer.parseInt(arrowID));
+                if (arrow == null || arrow.getText().equals("")) {
+                    // Don't add anything to rowTotal
+                }
+                else if (arrow.getText().equals("M")) {
+                    miss = true;
+                }
+                else if (arrow.getText().equals("X")) {
+                    rowET += 10;
+                }
+                else {
+                    rowET += Integer.parseInt((String) arrow.getText());
+                }
+            }
+
+            // Determining what to put in ET for this row
+            if (rowET != 0 || miss) {
+                currentET.setText(Integer.toString(rowET));
+            }
+            else {
+                currentET.setText("");
+            }
+
+            // RT calculations
+            if (i == 1) {
+                currentRT.setText(currentET.getText());
+            }
+            else {
+                String prevRTID = String.format("30%s", i - 1);
+                TextView prevRT = findViewById(Integer.parseInt(prevRTID));
+                int prevRTVal;
+                if (prevRT.getText().equals("")) {
+                     prevRTVal = 0;
+                }
+                else {
+                    prevRTVal = Integer.parseInt((String) prevRT.getText());
+                }
+                int RT = rowET + prevRTVal;
+
+                if (rowET != 0 || miss) {
+                    currentRT.setText(Integer.toString(RT));
+                }
+                else {
+                    currentRT.setText("");
+                }
+            }
+        }
     }
 }
